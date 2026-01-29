73b771f2979450b0796817cd7401a743b431b8bf
LUPUS001 <antoniodaw23@gmail.com>
2026-01-29 09:26:57 +0000

    3.2.4 Ventana modal

--- public/js/app.js
+++ public/js/app.js
@@ -15,4 +15,31 @@
     $(".closeInfoProduct").click(function (e) {
       infoProduct.modal('hide');
     });
+
+    // --- LÓGICA DEL CARRITO ---
+    const cartModal = $("#cart-modal");
+
+    $(".open-cart").click(function(event) {
+        event.preventDefault();
+        const id = $(this).data('id');
+        const href = `/cart/add/${id}`;
+        
+        $.get(href, function(data) {
+            // Rellenamos el modal con los datos del JSON
+            $(cartModal).find(".name").text(data.name);
+            $(cartModal).find("img").attr("src", "/img/" + data.photo);
+            $(cartModal).find("#quantity").val(data.quantity);
+            
+            // Abrimos el modal (Bootstrap 5)
+            const modalInstance = new bootstrap.Modal(document.getElementById('cart-modal'));
+            modalInstance.show();
+        });
+    });
+    
+    // Cerrar modal 
+    $(".closeCart").click(function() {
+        const modalEl = document.getElementById('cart-modal');
+        const modal = bootstrap.Modal.getInstance(modalEl);
+        if(modal) modal.hide();
+    });
 })();
